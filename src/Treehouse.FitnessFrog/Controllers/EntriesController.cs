ce361ca3c5d998edde935555172c2f84e7f8d6dd
Laurent Luypaert <laur.luyp@hotmail.com>
2019-06-18 16:00:05 +0200

    Made the View Strongly typed

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -40,14 +40,18 @@ namespace Treehouse.FitnessFrog.Controllers
         }
 
         public ActionResult Add()
-        {            
-            return View();
+        {
+            var entry = new Entry()
+            {
+                Date = DateTime.Today
+            };
+            return View(entry);
         }
 
         [HttpPost]
-        public ActionResult Add(DateTime? date, int? activityID, double? duration, Entry.IntensityLevel? intensity, bool? exclude, string notes)
+        public ActionResult Add(Entry entry)
         {
-           return View();
+           return View(entry);
         }
 
         public ActionResult Edit(int? id)
